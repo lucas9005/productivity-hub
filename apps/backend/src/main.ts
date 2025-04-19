fcb74c16cb8c140d8de95411dde20bb168993629
Lucas Ruggiero <lucas9005@gmail.com>
2025-04-19 07:15:00 -0300

    swagger setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+	const config = new DocumentBuilder().addBearerAuth().build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('swagger', app, document, {
+		swaggerOptions: { persistAuthorization: true, displayRequestDuration: true }
+	});
 	await app.listen(process.env.PORT ?? 3000);
 }
 
